5204751cb9cd44939dd8e7c04c4cdc139456d47c
Andrei Kropotoff <andreikropotoff@Andreis-MacBook-Pro.local>
2026-03-14 21:40:07 +0000

    refactoring and removing dead code

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/architech_logo.png';
-
-const Header = () => (
-  <div style={{ textAlign: 'center' }}>
-    <img src={logo} alt="Architech Logo" className="architech-logo" />
-  </div>
-);
-
-export default Header;
